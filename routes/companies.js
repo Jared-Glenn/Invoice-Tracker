6e5f00238af1adb2448408082cc45a1a15d48686
Jared <jared_glenn@yahoo.com>
2024-01-03 20:15:57 -0700

    bug with db not starting, investigating

---
+++ routes/companies.js
@@ -0,0 +1,17 @@
+// Routes for the company Invoice Tracker.
+
+const express = require("express");
+const router = express.Router();
+const db = require("../db");
+
+// Get full list of companies.
+router.get('/', async (req, res) => {
+    const results = await db.query(`SELECT * FROM companies`);
+    return res.json(results.rows)
+})
+
+
+
+
+
+module.exports = router;
