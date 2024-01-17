2336077670717bf674c4a2590e002a02c52730cb
Jared <jared_glenn@yahoo.com>
2024-01-16 22:20:07 -0700

    added jest tests

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -7,14 +7,33 @@ const db = require('../db');
 let testCompany;
 beforeEach(async () => {
     const result = await db.query(`INSERT INTO companies (code, name, description)
-    VALUES ('apple', 'Apple Computers', "Created iOS.')
+    VALUES ('apple', 'Apple Computers', 'Created iOS.')
     RETURNING code, name, description`);
     testCompany = result.rows[0];
+})
 
-    describe("HOPE THIS WORKS", () => {
-        testCompany("BLAH", () => {
-            console.log(testCompany);
-            expect(1).toBe(1);
-        })
+afterEach(async () => {
+    // Clean up database after each test
+    await db.query('DELETE FROM companies');
+});
+
+afterAll(async () => {
+    await db.end();
+})
+
+describe("GET /companies", () => {
+    test("Get a list with one company", async () => {
+        const res = await request(app).get('/companies');
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual([testCompany]);
+    })
+})
+
+describe("GET /companies/:code", () => {
+    test("Get a specific company", async () => {
+        const res = await request(app).get('/companies/apple');
+        expect(res.statusCode).toBe(200);
+        console.log(testCompany);
+        expect(res.body).toEqual( {"company": testCompany, "invoices": []});
     })
 })
